3b1351e91fd79f3142639be8328086010b00eb8d
Jaime Kumar <jaimelacree@gmail.com>
2023-09-08 18:53:15 +0100

    video playsInline 2

--- src/Homepage.js
+++ src/Homepage.js
@@ -225,7 +225,10 @@ export default function Homepage({scrolled, forceToTop, makeNavBlack, makeNavWhi
   return (
     <div className='page' id='home'>
       <div className="section" style={{overflowY: 'hidden'}}>
-        <video id='vid' className='homeVid' type="video/mp4" src={vid} autoPlay={true} loop muted playsInline></video>
+      <video autoplay loop muted playsinline class="homeVid">
+        <source src={vid} type="video/mp4" />
+      </video>
+        {/* <video id='vid' className='homeVid' type="video/mp4" src={vid} autoPlay={true} loop muted playsInline></video> */}
       </div>
       <div className="section" id='collections'>
         {collections.map((c, i) => {
